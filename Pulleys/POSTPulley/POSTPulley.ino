6c96e732f531aa9de9df92d550cb06bf83e9135c
Gustav2 <gustavnybro@protonmail.com>
2022-12-21 23:59:54 +0100

    added initial post-request pulley reciever

---
+++ Pulleys/POSTPulley/POSTPulley.ino
@@ -0,0 +1,61 @@
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <Regexp.h>
+
+
+
+// WiFi credentials
+#include "Secret.h"
+const char *ssid = SECRET_SSID;
+const char *password = SECRET_PASS;
+char c;
+String readString = String(100);
+WiFiServer wifiServer(80);
+
+
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
+
+  WiFi.begin(ssid, password);
+  WiFi.mode(WIFI_STA);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting..");
+  }
+
+  Serial.print("Connected to WiFi. IP:");
+  Serial.println(WiFi.localIP());
+  wifiServer.begin();
+}
+
+
+void loop() {
+  WiFiClient client = wifiServer.available();
+
+
+  if (client) {
+    Serial.println("Client connected");
+
+    while (client.connected()) {
+
+      while (client.available() > 0) {
+
+        char c = client.read();
+        if (readString.length() < 100) {
+          readString.concat(c);
+        }
+        Serial.print(c);
+
+
+        if (c == '\n') {
+
+          delay(50);
+          readString = "";
+          client.stop();
+        }
+      }
+    }
+  }
+}
