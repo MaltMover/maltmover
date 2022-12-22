dc7ab7fb933979a8e61c2d1d4934d7ff3d638405
DNIIBOY <daniel.nettelfield@gmail.com>
2022-12-22 01:43:38 +0100

    Add success_map to request_handler.py, allowing easy way to figure out which pulleys aren't responding

--- Server/main.py
+++ Server/main.py
@@ -5,16 +5,16 @@ from request_handler import RequestsHandler
 
 # All measurements are in decimeters (10 cm)
 SIZE = [10, 10, 10]
-ROPE_LENGTH = 50
+ROPE_LENGTH = 15
 MAX_SPEED = 10
 
 # Ips of the pulley-systems
-IPS = [
-    "192.168.172.1",
+IPS = (
+    "192.168.1.64",
     "192.168.172.2",
     "192.168.172.3",
     "192.168.172.4"
-]
+)
 
 
 def main():
@@ -23,12 +23,12 @@ def main():
     space.add_pulley(Pulley(Point(SIZE[0], 0, SIZE[2]), ROPE_LENGTH, MAX_SPEED))
     space.add_pulley(Pulley(Point(0, SIZE[1], SIZE[2]), ROPE_LENGTH, MAX_SPEED))
     space.add_pulley(Pulley(Point(SIZE[0], SIZE[1], SIZE[2]), ROPE_LENGTH, MAX_SPEED))
-    target1 = Point(3, 3, SIZE[2])
-    space.update_lengths(target1, 1)
-    target2 = Point(4, 3, SIZE[2])
-    space.update_lengths(target2, 0.2)
+    target = Point(10, 10, SIZE[2])
+    space.update_lengths(target, 2)
     for pulley in space.pulleys:
         print(pulley.length)
+    requests_handler = RequestsHandler(IPS)
+    print(requests_handler.set_pulleys(space.pulleys, 2))
 
 
 if __name__ == '__main__':
--- Server/request_handler.py
+++ Server/request_handler.py
@@ -1,5 +1,4 @@
 from pulley import Pulley
-import json
 import requests
 import threading
 
@@ -14,16 +13,18 @@ IPS = (
 
 
 class RequestsHandler:
-    def __init__(self, addresses: tuple[str, ...]):
+    def __init__(self, addresses: tuple[str, ...] | list[str, ...]):
         self.addresses = addresses
         self.threads = []
         self.response_count = 0
-        self.success_count = 0
+        self.success_map = []  # Stores boolean map of success for each request
 
     def __repr__(self):
         return f"RequestsHandler{self.addresses}"
 
-    def set_pulleys(self, pulleys: list[Pulley], time: int | float) -> bool:
+    def set_pulleys(self, pulleys: list[Pulley], time: int | float) -> list[bool]:
+        if len(pulleys) != len(self.addresses):
+            raise ValueError("Number of pulleys must equal number of addresses")
         data = []
         # Set data for each pulley
         for pulley in pulleys:
@@ -34,26 +35,33 @@ class RequestsHandler:
                 }
             )
         # Send first request to all pulleys
-        if not self.send_requests(data):
-            return False
+        if not self.send_requests(data, request_num=0):
+            return self.success_map
 
         # If all succeeded, send second request to all pulleys
-        self.send_requests([{"Run": True}] * len(self.addresses))
+        self.send_requests([{"Run": True}] * len(self.addresses), request_num=1)
+        return self.success_map
 
-    def send_requests(self, data: list[dict]):
+    def send_requests(self, data: list[dict], timeout: int | float = 3, request_num=-1) -> bool:
         self.response_count = 0
-        self.success_count = 0
+        # Reset success map, all False
+        self.success_map = [
+            [False for _ in range(len(self.addresses))],
+            [False for _ in range(len(self.addresses))]
+        ]
 
         # Create one thread for each client
+        pulley_num = 0
         for address, d in zip(self.addresses, data):
-            self.threads.append(threading.Thread(target=self.send_request, args=(address, d)))
+            self.threads.append(threading.Thread(target=self.send_request, args=(address, d, timeout, request_num, pulley_num)))
+            pulley_num += 1
 
         # Start all threads
         for thread in self.threads:
             thread.start()
 
         # Wait for all threads to finish
-        while self.response_count < len(self.addresses):
+        while not self.response_count == len(self.addresses):
             sleep(0.1)
 
         # Join all threads
@@ -62,20 +70,20 @@ class RequestsHandler:
         self.threads = []
 
         # Check if all requests were successful
-        return self.success_count == self.response_count == len(self.addresses)
+        return all(self.success_map[request_num])
 
-    def send_request(self, address: str, data: dict) -> bool:
+    def send_request(self, address: str, data: dict, timeout: int | float, request_num, pulley_num) -> bool:
         """
         Send request to one client, and return success as bool
         """
         try:
-            response = requests.post(f"http://{address}/", json=data, timeout=3)
-        except (requests.exceptions.InvalidSchema, requests.exceptions.ConnectTimeout):
+            response = requests.post(f"http://{address}/", json=data, timeout=timeout)
+        except (requests.exceptions.InvalidSchema, requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
             self.response_count += 1
             return False
         self.response_count += 1
         if response.status_code == 200:
-            self.success_count += 1
+            self.success_map[request_num][pulley_num] = True
             return True
         return False
 
