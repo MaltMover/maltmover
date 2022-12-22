9ff6c7a1cb453f5366a2c3f26256c0c8ef809216
Gustav2 <gustavnybro@protonmail.com>
2022-12-22 14:58:48 +0100

    Added requst parsing

--- Pulleys/POSTPulley/POSTPulley.ino
+++ Pulleys/POSTPulley/POSTPulley.ino
@@ -1,6 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-
+#include <ArduinoJson.h>
 
 // WiFi credentials
 #include "Secret.h"
@@ -13,41 +13,57 @@ ESP8266WebServer server(80);
 void setup() {
 
   Serial.begin(9600);
-  WiFi.begin(ssid, password);  
+  WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {  
+  while (WiFi.status() != WL_CONNECTED) {
 
     delay(500);
     Serial.println("Waiting to connect...");
   }
 
   Serial.print("IP address: ");
-  Serial.println(WiFi.localIP()); 
+  Serial.println(WiFi.localIP());
 
-  server.on("/", handleBody);  
+  server.on("/", handleBody);
 
-  server.begin(); 
+  server.begin();
   Serial.println("Server listening");
 }
 
 void loop() {
 
-  server.handleClient();  
+  server.handleClient();
 }
 
-void handleBody() {  
+void handleBody() {
+  DynamicJsonDocument doc(1024); // Init json buffer
 
-  if (server.hasArg("plain") == false) {  
+  if (server.hasArg("plain") == false) {
 
     server.send(200, "text/plain", "Body not received");
     return;
   }
 
-  String message = "Body received:\n";
+  String message;
   message += server.arg("plain");
   message += "\n";
 
   server.send(200, "text/plain", message);
   Serial.println(message);
-}
 
+  // Deserialize json and return if error
+  DeserializationError error = deserializeJson(doc, message);
+  if (error)
+    return;
+  if (doc.containsKey("run")) {
+    Serial.println("run");
+
+  }
+  else if (doc.containsKey("length") && doc.containsKey("time")) {
+    double length = doc["length"];
+    double time = doc["time"];
+
+    Serial.println(time);
+    
+  }
+}
