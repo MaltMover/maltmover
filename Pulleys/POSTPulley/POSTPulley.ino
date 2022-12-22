a7c3d1963a1feaa41defd3ec0a4a598c6a9d4b1f
Gustav2 <gustavnybro@protonmail.com>
2022-12-22 14:19:43 +0100

    Rewrote ESP code, now works

--- Pulleys/POSTPulley/POSTPulley.ino
+++ Pulleys/POSTPulley/POSTPulley.ino
@@ -1,61 +1,53 @@
-
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <Regexp.h>
-
+#include <ESP8266WebServer.h>
 
 
 // WiFi credentials
 #include "Secret.h"
 const char *ssid = SECRET_SSID;
 const char *password = SECRET_PASS;
-char c;
-String readString = String(100);
-WiFiServer wifiServer(80);
 
+
+ESP8266WebServer server(80);
 
 void setup() {
-  Serial.begin(9600);
-  delay(1000);
 
-  WiFi.begin(ssid, password);
-  WiFi.mode(WIFI_STA);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting..");
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);  
+
+  while (WiFi.status() != WL_CONNECTED) {  
+
+    delay(500);
+    Serial.println("Waiting to connect...");
   }
 
-  Serial.print("Connected to WiFi. IP:");
-  Serial.println(WiFi.localIP());
-  wifiServer.begin();
-}
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP()); 
 
+  server.on("/", handleBody);  
+
+  server.begin(); 
+  Serial.println("Server listening");
+}
 
 void loop() {
-  WiFiClient client = wifiServer.available();
 
-
-  if (client) {
-    Serial.println("Client connected");
-
-    while (client.connected()) {
-
-      while (client.available() > 0) {
-
-        char c = client.read();
-        if (readString.length() < 100) {
-          readString.concat(c);
-        }
-        Serial.print(c);
-
-
-        if (c == '\n') {
-
-          delay(50);
-          readString = "";
-          client.stop();
-        }
-      }
-    }
-  }
+  server.handleClient();  
 }
+
+void handleBody() {  
+
+  if (server.hasArg("plain") == false) {  
+
+    server.send(200, "text/plain", "Body not received");
+    return;
+  }
+
+  String message = "Body received:\n";
+  message += server.arg("plain");
+  message += "\n";
+
+  server.send(200, "text/plain", message);
+  Serial.println(message);
+}
+
