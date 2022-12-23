5cde01a76e3e4bea9a7864fec4b89057165a1360
Gustav2 <gustavnybro@protonmail.com>
2022-12-23 02:09:02 +0100

    added speed calculation

--- Pulleys/POSTPulley/Pulleys.h
+++ Pulleys/POSTPulley/Pulleys.h
@@ -1,5 +1,6 @@
 #include "core_esp8266_features.h"
 #include <Stepper.h>
+#include <math.h>
 
 const double stepsPrRev = 2047; 
 const double revsPrDM = 2; 
@@ -9,10 +10,15 @@ Stepper PulleyStepper(stepsPrRev, 14, 13, 12, 15);
 void runPulleys(double preparedLength, double preparedTime, double* mem_currentLength) {
   double lenToMove = preparedLength - *mem_currentLength;
   
-  PulleyStepper.setSpeed(10);
+  
 
   double revAmount = revsPrDM * lenToMove;
   double stepAmount = revAmount * stepsPrRev;
+  double speed = (abs(revAmount) / abs(preparedTime)) * 60;
+
+
+
+  PulleyStepper.setSpeed(round(speed));
 
   Serial.print("current length: ");
   Serial.println(*mem_currentLength);
@@ -24,13 +30,23 @@ void runPulleys(double preparedLength, double preparedTime, double* mem_currentL
   Serial.println(stepAmount);
   Serial.println();
 
-  for (int i = 0; i < stepAmount; i++) {
-    PulleyStepper.step(1);
+  
+  for (int i = 0; i < abs(stepAmount); i++) {
+    if (stepAmount > 0) {
+      PulleyStepper.step(1);
+    } else if (stepAmount < 0) {
+      PulleyStepper.step(-1);
+    } else {
+      break;
+    }     
+
     if (i % 25 == 0) {
       yield();
       delay(1);
     }
   }
+  
+  
 
   *mem_currentLength = preparedLength;
   //delay(preparedTime * 1000);
