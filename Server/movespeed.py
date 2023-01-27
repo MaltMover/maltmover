d8444ad1818b4f9c532f5e7e120e9b17affad631
DNIIBOY <daniel.nettelfield@gmail.com>
2023-01-27 22:50:51 +0100

    More fancy regression

--- Server/movespeed.py
+++ Server/movespeed.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 import pyperclip
 
@@ -23,8 +25,8 @@ space = Space(size[0], size[1], size[2], edge_limit=0)
 for p in pulleys:
     space.add_pulley(p)
 
-start = Point(10, 50, 100)
-end = Point(85, 90, 100)
+start = Point(10, 20, 100)
+end = Point(90, 30, 100)
 
 space.update_lengths(start, -1, check_limit=False)
 for p in space.pulleys:
@@ -50,34 +52,55 @@ for i in range(point_count + 1):
 
 lens = [len0, len1, len2, len3]
 rows = []
-x = list(range(len(len0)))
-y = len2
-print(len2)
-model = numpy.poly1d(numpy.polyfit(x, y, 2))
-a = float(model[2])
-b = float(model[1])
-c = float(model[0])
+# x = list(range(len(len0)))
+# y = len2
+# print(len2)
+# model = numpy.poly1d(numpy.polyfit(x, y, 2))
+# a = float(model[2])
+# b = float(model[1])
+# c = float(model[0])
 # c = float(len2[0])
-r2 = r2_score(y, model(x))
-print(a, b, c)
-print(r2)
-line = numpy.linspace(0, len(len0), 100)
-plt.plot(numpy.polyval(model, line))
+# r2 = r2_score(y, model(x))
+# print(a, b, c)
+# print(r2)
+# line = numpy.linspace(0, len(len0), 100)
+# plt.plot(numpy.polyval(model, line))
+# plt.show()
+
+for i in range(len(len0)):
+    row = []
+    for length in lens:
+        row.append(str(length[i]).replace(".", ","))
+    rows.append(row)
+
+rows = ["\t".join(row) for row in rows]
+table = "\r\n".join(rows)
+pyperclip.copy(table)
+
+# print(sums)
+plt.plot(len0)
+plt.plot(len1)
+plt.plot(len2)
+plt.plot(len3)
 plt.show()
 
-# for i in range(len(len0)):
-#     row = []
-#     for length in lens:
-#         row.append(str(length[i]).replace(".", ","))
-#     rows.append(row)
-#
-# rows = ["\t".join(row) for row in rows]
-# table = "\r\n".join(rows)
-# pyperclip.copy(table)
-#
-# print(sums)
-# plt.plot(len0)
-# plt.plot(len1)
-# plt.plot(len2)
-# plt.plot(len3)
-# plt.show()
+
+# create the training data
+x = [[int(i)] for i in range(len(len2))]
+print(x)
+y = len2
+
+# transform the data to include polynomial features
+poly = PolynomialFeatures(degree=2)
+x_poly = poly.fit_transform(x)
+
+# create and fit the model with specified y-intercept
+model = LinearRegression(fit_intercept=True)
+model.fit(x_poly, y)
+model.intercept_ = len2[0]  # specify y-intercept
+
+print(model.coef_)
+# predict new values
+x_new = [[100]]
+y_new = model.predict(poly.transform(x_new))
+print(y_new)
