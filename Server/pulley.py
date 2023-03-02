e1d216117bd8af384755644dfa29b492668a4860
DNIIBOY <daniel.nettelfield@gmail.com>
2023-03-02 20:22:52 +0100

    Big brain math for calculating velocity with known speed

--- Server/pulley.py
+++ Server/pulley.py
@@ -19,9 +19,9 @@ class Pulley:
         self.max_length = round(float(max_length), 2)  # in decimeters (10 cm)
         self.max_speed = round(float(max_speed), 2)  # in decimeters per second (10 cm/s)
         self.max_acceleration = round(float(max_acceleration), 2)  # in decimeters per second squared (10 cm/s^2)
-        self.length = 0  # in decimeters (10 cm)
-        self.speed = 0  # in decimeters per second (10 cm/s)
-        self.acceleration = 0  # in decimeters per second squared (10 cm/s^2)
+        self._length = 0  # in decimeters (10 cm)
+        self._speed = 0  # in decimeters per second (10 cm/s)
+        self._acceleration = 0  # in decimeters per second squared (10 cm/s^2)
 
     def __repr__(self):
         return f'Pulley(Location: ({self.x}, {self.y}, {self.z}), length: {self.length},' \
@@ -30,37 +30,74 @@ class Pulley:
     def __lt__(self, other):
         return self.location < other.location
 
-    def set_length(self, length: int | float) -> float:
+    @property
+    def length(self) -> float:
+        return self._length
+
+    @property
+    def speed(self) -> float:
+        return self._speed
+
+    @property
+    def acceleration(self) -> float:
+        return self._acceleration
+
+    @length.setter
+    def length(self, length: int | float):
         """
         Sets the length of the rope attached to the pulley.
         :param length: The new length of the rope attached to the pulley.
-        :return: The minimum time needed to change the length of the rope.
         """
         if length > self.max_length:
             raise ValueError(f'Length cannot be greater than {self.max_length} dm.')
-        time = abs(self.length - length) / self.max_speed
-        self.length = round(float(length), 2)
-        return time
+        self._length = round(float(length), 2)
 
-    def set_speed(self, speed: int | float):
+    @speed.setter
+    def speed(self, speed: int | float):
         """
         Sets the speed of the pulley.
         :param speed: The new speed of the pulley.
         """
         if speed > self.max_speed:
             raise ValueError(f'Speed cannot be greater than {self.max_speed} dm/s.')
-        self.speed = round(float(speed), 2)
+        self._speed = round(float(speed), 2)
 
-    def set_time(self, time: int | float):
+    @acceleration.setter
+    def acceleration(self, acceleration: int | float):
         """
-        Sets the speed of the pulley, given the time to move.
-        :param time: The new time of the pulley.
+        Sets the acceleration of the pulley.
+        :param acceleration: The new acceleration of the pulley.
         """
+        if acceleration > self.max_acceleration:
+            raise ValueError(f'Acceleration cannot be greater than {self.max_acceleration} dm/s^2.')
+        self._acceleration = round(float(acceleration), 2)
+
+    def make_move(self, new_length: int | float, time: int | float) -> tuple:
+        """
+        Makes a move with the pulley, and calculates the new speed and acceleration.
+        :param new_length: The new rope length.
+        :param time: The time to move.
+        """
+        if new_length > self.max_length:
+            raise ValueError(f'Length cannot be greater than {self.max_length} dm.')
+        if time <= 0:
+            raise ValueError(f'Time must be greater than 0.')
+
+        # Calculate the new speed with cool math
+        self.speed = -((self.max_acceleration * (self.max_acceleration * time ** 2 - 4 * new_length)) ** 0.5 - self.max_acceleration * time) / 2
+        self.acceleration = self.max_acceleration
+        self.length = new_length
+
+        return self.speed, self.acceleration
 
 
 def main():
-    pulley = Pulley(Point(0, 0, 0), 10, 10)
+    pulley = Pulley(Point(0, 0, 0), max_length=50, max_speed=10, max_acceleration=2)
     print(pulley)
+    print(pulley.make_move(50, 12.5))
+    print(pulley.make_move(50, 10))
+    print(pulley.make_move(50, 20))
+    print(pulley.make_move(12.5, 5))
 
 
 if __name__ == '__main__':
