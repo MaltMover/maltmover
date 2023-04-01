6c942e35390b2943ceba4b2da5acd8e8eeb22fa7
DNIIBOY <daniel.nettelfield@gmail.com>
2023-04-01 22:25:52 +0200

    try to read wifi addresses

--- WiFiHost/WiFiHost.ino
+++ WiFiHost/WiFiHost.ino
@@ -1,11 +1,14 @@
 #include <ESP8266WiFi.h>
 
+extern "C" {
+  #include<user_interface.h>
+}
+
 const char *ssid = "MaltMover";
 const char *password = "maltmover123";
 
+IPAddress gateway(192, 168, 4, 0);
 IPAddress local_IP(192, 168, 4, 1);
-
-IPAddress gateway(192, 168, 4, 9);
 IPAddress subnet(255, 255, 255, 0);
 
 void setup()
@@ -22,7 +25,38 @@ void setup()
 }
 
 void loop() {
-  Serial.print("[Server Connected] ");
-  Serial.println(WiFi.softAPIP());
+  client_status();
+  delay(5000);
+}
+
+void client_status() {
+  unsigned char number_client;
+  struct station_info *stat_info;
+  
+  struct ip4_addr *IPaddress;
+  IPAddress address;
+  int i=1;
+  
+  number_client= wifi_softap_get_station_num();
+  stat_info = wifi_softap_get_station_info();
+  
+  Serial.print(" Total Connected Clients are = ");
+  Serial.println(number_client);
+  
+  while (stat_info != NULL) {
+  
+    IPaddress = &stat_info->ip;
+    address = IPaddress->addr;
+    
+    Serial.print("client= ");
+    Serial.print(i);
+
+    Serial.print(" IP adress is = ");
+    Serial.print((address));
+    
+    stat_info = STAILQ_NEXT(stat_info, next);
+    i++;
+    Serial.println();
+  }
   delay(500);
 }
