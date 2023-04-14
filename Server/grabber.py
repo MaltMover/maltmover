2ee1700805d7ff79dc6355cf22dc87b36d225102
DNIIBOY <daniel.nettelfield@gmail.com>
2023-04-14 18:00:20 +0200

    Add comments to grabber.py (#28)

--- Server/grabber.py
+++ Server/grabber.py
@@ -2,17 +2,26 @@ from point import Point, Waypoint
 
 
 class Grabber:
+    """
+    Grabber class represent a grabber, with a location in space.
+    :param corner_distance: Perpendicular distance from the center of the grabber
+     to the corners (see README.md).
+    """
     def __init__(self, corner_distance=0.0):
-        self.is_open = False
+        self.is_open = False  # True if the grabber is open, False if it is closed
         self.corner_distance = corner_distance
-        self._location: Point | Waypoint = Point(0, 0, 0)
-        self.corners: list[Point, ...] = []
+        self.corners: list[Point, ...] = []  # The corners of the grabber
+        self.location: Point | Waypoint = Point(0, 0, 0)  # The location of the grabber
 
     def _update_corners(self):
+        """
+        Updates the corner locations based on the location of the grabber.
+        """
         if self.location is None:
+            # If the location is None, set the corners to None
             self.corners = [None] * 4
             return
-        self.corners = [
+        self.corners = [  # Offset the corners based on their position relative to the center
             Point(self.location.x - self.corner_distance, self.location.y - self.corner_distance, self.location.z),
             Point(self.location.x + self.corner_distance, self.location.y - self.corner_distance, self.location.z),
             Point(self.location.x - self.corner_distance, self.location.y + self.corner_distance, self.location.z),
@@ -25,5 +34,8 @@ class Grabber:
 
     @location.setter
     def location(self, value: Point | None) -> None:
+        """
+        Sets the location of the grabber, and updates the corners.
+        """
         self._location = value
         self._update_corners()
